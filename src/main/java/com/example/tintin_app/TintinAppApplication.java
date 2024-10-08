a8d158ef97d61aea80e1cece83a45c38b9de4298
tintin <tintin.vocard@gmail.com>
2024-10-08 09:25:17 +0200

    initial commit

---
+++ src/main/java/com/example/tintin_app/TintinAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tintin_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TintinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TintinAppApplication.class, args);
+	}
+
+}
