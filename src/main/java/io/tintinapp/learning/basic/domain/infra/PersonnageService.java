a048b834b8e13e1ca2e4ddd670c72bfbc8e7dae1
tintin <tintin@host-001.lan>
2024-10-19 08:37:32 +0200

    refacto PersonnageServiceTest

--- src/main/java/io/tintinapp/learning/basic/domain/infra/PersonnageService.java
+++ src/main/java/io/tintinapp/learning/basic/domain/infra/PersonnageService.java
@@ -1,9 +1,11 @@
 package io.tintinapp.learning.basic.domain.infra;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import io.tintinapp.learning.basic.domain.infra.entity.Accessoire;
 import io.tintinapp.learning.basic.domain.infra.entity.Personnage;
 import lombok.RequiredArgsConstructor;
 
@@ -15,5 +17,23 @@ public class PersonnageService {
 
   public List<Personnage> getAll() {
     return personnageRepository.findAll();
+  }
+
+  public void creer(Personnage... personnage) {
+    personnageRepository.saveAll(List.of(personnage));
+  }
+
+  public List<String> getAllAccessoiresDesPersonnages() {
+    return getAll().stream()
+        .flatMap(perso -> perso.getAccessoires().stream())
+        .map(acc -> acc.getNom())
+        .toList();
+  }
+
+  public Optional<Accessoire> someAccessoire() {
+    return getAll().stream()
+      .findAny()
+      .map(perso -> perso.getAccessoires().stream().findAny())
+        .orElseThrow();
   }
 }
--- src/test/java/io/tintinapp/learning/basic/PersonnageServiceTest.java
+++ src/test/java/io/tintinapp/learning/basic/PersonnageServiceTest.java
@@ -1,59 +1,75 @@
 package io.tintinapp.learning.basic;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 
+import org.hibernate.LazyInitializationException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import io.tintinapp.learning.MesBuilders;
-import io.tintinapp.learning.basic.domain.infra.AccessoireRepo;
-import io.tintinapp.learning.basic.domain.infra.PersonnageRepository;
 import io.tintinapp.learning.basic.domain.infra.PersonnageService;
 import io.tintinapp.learning.basic.domain.infra.entity.Personnage;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
 @Slf4j
+@TestInstance(Lifecycle.PER_CLASS)
 // @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PersonnageServiceTest {
 
 	@Autowired
 	private PersonnageService personnageService;
 
-	@Autowired
-	private PersonnageRepository personnageRepository;
-
-	@Autowired
-	private AccessoireRepo accessoireRepository;
+	@BeforeAll
+	public void setUp() {
+		List<Personnage> allPersonnages = List.of(
+				MesBuilders.unPersonnage(
+						"Tintin",
+						MesBuilders.unAccessoire("besace")),
+				MesBuilders.unPersonnage(
+						"Milou",
+						MesBuilders.unAccessoire("musoliere")));
+		personnageService.creer(allPersonnages.toArray(new Personnage[0]));
+	}
 
 	@Test
-	@DisplayName("step 2 (wip) : constater probleme N+1")
-	public void constaterProblemeNPlus1() {
-		// given : j'ai 2 personnage ayant chacun 1 accessoires
-		Personnage tintin = MesBuilders.unPersonnage(
-				"Tintin",
-				MesBuilders.unAccessoire("besace"));
-		Personnage milou = MesBuilders.unPersonnage(
-				"Milou",
-				MesBuilders.unAccessoire("musoliere"));
+	@DisplayName("step 0 : Lazy initialisation probleme")
+	public void step0LaziInit() {
+		// given : ...
+		// when : ...
 
-		tintin = personnageRepository.save(tintin);
-		milou = personnageRepository.save(milou);
-
-		// when : je recupere tous les personnages
-		List<Personnage> allPersonnages = personnageService.getAll();
-
-		List<String> accessoireNoms = allPersonnages.stream()
-				.flatMap(perso -> perso.getAccessoires().stream())
-				.map(acc -> acc.getNom())
-				.toList();
-		log.info(accessoireNoms.toString());
-
-		assertThat(accessoireNoms).hasSize(2);
+		assertThrows(LazyInitializationException.class,
+			 () -> personnageService.someAccessoire());
 	}
+
+	@Test
+	@Transactional
+	@DisplayName("step 0 : Lazy initialisation probleme - corrige")
+	public void step0LaziInitCorrige() {
+		// given : ...
+		// when : ...
+
+		assertThat(personnageService.someAccessoire()).isPresent();
+	}
+
+	// @Test
+	// @DisplayName("step 2 (wip) : constater probleme N+1")
+	// public void constaterProblemeNPlus1() {
+	// 	// given : j'ai 2 personnage ayant chacun 1 accessoires
+
+	// 	// when : je cree tous les personnages
+	// 	List<String> accessoireNoms = personnageService.getAllAccessoiresDesPersonnages();
+
+	// 	log.info(accessoireNoms.toString());
+	// 	assertThat(accessoireNoms).hasSize(2);
+	// }
 }
